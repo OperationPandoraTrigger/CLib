01f8757ce570b6a6b59768431c2a61f5feeae4da
jokoho48 <hoffmann.jonas95@gmail.com>
2017-07-23 17:50:46 +0200

    wups

--- extensions/CLib/CLibScriptCaller/DllEntry.cs
+++ extensions/CLib/CLibScriptCaller/DllEntry.cs
@@ -12,11 +12,33 @@ namespace CLibScriptCaller
 {
     public class DllEntry
     {
+#if WIN64
+        [DllExport("RVExtensionVersion")]
+#else
+		[DllExport("_RVExtensionVersion@6", CallingConvention.StdCall)]
+#endif
+        public static void RVExtensionVersion(StringBuilder output, int outputSize)
+        {
+            outputSize--;
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            try
+            {
+                string location = executingAssembly.Location;
+                if (location == null)
+                    throw new Exception("Assembly location not found");
+                output.Append(FileVersionInfo.GetVersionInfo(location).FileVersion);
+            }
+            catch (Exception e)
+            {
+                output.Append(e.Message);
+            }
+        }
         // arma Default Entry Point
 #if WIN64
+        [DllExport("RVExtension")]
 #else
+		[DllExport("_RVExtension@12", CallingConvention.StdCall)]
 #endif
-        [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
         public static void RVExtension(StringBuilder output, int outputSize, [MarshalAs(UnmanagedType.LPStr)] string input)
         {
             if (input != "version")
