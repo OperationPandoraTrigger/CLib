ee22db3e53a1574090927ec0243b8439c1f4280e
Krzmbrzl <krzmbrzl@gmail.com>
2019-05-01 20:29:31 +0200

    Added a pboName-h file for creating pboNames containing the current version

---
+++ addons/CLib/pboName.h
@@ -0,0 +1,5 @@
+// Diese Datei muss durch den preprocessor gejagt werden, damit der Name der PBO
+// richtig hier drin steht
+// Da die Mod nachher auf einem Linux-Server laufen soll, muss der PBO-Name kmoplett kleingeschrieben sein
+#include "CLib_macros.hpp"
+pboName=QUOTE(clib_v##VERSION)
